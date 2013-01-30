4a22569af0a3ce972326dd99cb91d7cdcdd5a00e
James <james@jarofgreen.co.uk>
2013-01-30 16:02:50 +0000

    Add Google Analytics tracking to the list screens

--- app/src/uk/co/jarofgreen/cityoutdoors/UI/BaseListActivity.java
+++ app/src/uk/co/jarofgreen/cityoutdoors/UI/BaseListActivity.java
@@ -1,5 +1,7 @@
 package uk.co.jarofgreen.cityoutdoors.UI;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 import uk.co.jarofgreen.cityoutdoors.R;
 import android.app.ListActivity;
 import android.content.SharedPreferences;
@@ -9,6 +11,20 @@ import android.widget.TextView;
 public class BaseListActivity extends ListActivity {
 
 
+	private boolean analyticsEnabled = true;
 
+	@Override
+	public void onStart() {		
+		super.onStart();
+		SharedPreferences settings=PreferenceManager.getDefaultSharedPreferences(this);
+		analyticsEnabled = (settings.getString("analyticsEnabled", "yes").compareTo("yes") == 0);
+		if (analyticsEnabled) EasyTracker.getInstance().activityStart(this);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if (analyticsEnabled) EasyTracker.getInstance().activityStop(this);
+	}
 	
 }
